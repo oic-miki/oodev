89a7a99496ee52361f1be3b9446e73261e508b28
katsuki-sys <C0035@oic.jp>
2022-06-16 15:44:31 +0900

    Merge branch '2022_01' of https://github.com/oic-miki/oodev into 2022_01

--- src/oodev/C0035.java
+++ src/oodev/C0035.java
@@ -1,5 +1,13 @@
 package oodev;
 
 public class C0035 {
-   
+	public String name;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
 }
