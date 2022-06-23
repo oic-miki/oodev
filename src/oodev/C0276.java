893038e57490345b5ef6e73a84c1e0053e644f3d
C0276 <C0276@5N38.oic.jp>
2022-06-23 14:48:03 +0900

    Merge branch '2022_01' of https://github.com/oic-miki/oodev into 2022_01

--- src/oodev/C0276.java
+++ src/oodev/C0276.java
@@ -1,5 +1,16 @@
 package oodev;
 
 public class C0276 {
+	
+	private String name;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
 
 }
